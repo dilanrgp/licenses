7fd16850100c0f1c3cadfed3b6c43167bd75c520
Dilan Gonzalez <dgonzalez@teldat.com>
2025-09-03 16:29:34 +0200

    Comentando el css que genera error

--- src/app/shared/components/ckeditor/ckeditor.component.ts
+++ src/app/shared/components/ckeditor/ckeditor.component.ts
@@ -3,7 +3,7 @@ import { Component, input, signal } from '@angular/core';
 import { CKEditorModule } from '@ckeditor/ckeditor5-angular';
 import CustomEditor from '@shared/editors/ckeditor-custom';
 // import '@ckeditor/ckeditor5-theme-lark/theme/index.css';
-import '@ckeditor/ckeditor5-build-classic/build/ckeditor.css';
+// import '@ckeditor/ckeditor5-build-classic/build/ckeditor.css';
 
 @Component({
   selector: 'app-ckeditor',
