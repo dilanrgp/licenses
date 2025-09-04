fdad416ad085e5a2812df1fb0b3159e7be4cc6aa
Dilan Gonzalez <dilanrgp@gmail.com>
2025-09-04 17:12:20 +0200

    refactor: move license template modal to general settings

--- src/app/components/table-list/license-template-table/license-template-table.component.ts
+++ src/app/components/table-list/license-template-table/license-template-table.component.ts
@@ -1,42 +1,52 @@
-import { Component, effect, inject, input, linkedSignal, signal } from '@angular/core';
+import { Component, effect, inject, input, linkedSignal, output } from '@angular/core';
 import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
-import { LicenseTemplateModalComponent } from '@components/license-template-modal/license-template-modal.component';
 import { ApiLicenseTemplateResponse, LicenseTemplate } from '@interfaces/license-template.interface';
 import { ChangeLanguagePipe } from '@pipes/change-language.pipe';
-import { ModalService } from '@services/modal.service';
 
 @Component({
   selector: 'app-license-template-table',
-  imports: [ChangeLanguagePipe, LicenseTemplateModalComponent],
+  imports: [ChangeLanguagePipe],
   templateUrl: './license-template-table.component.html',
 })
 export class LicenseTemplateTableComponent {
-  
   sanitizer = inject(DomSanitizer);
-  modalService = inject(ModalService);
 
   licenseTemplateData = input.required<ApiLicenseTemplateResponse>();
+  updatedTemplate = input<LicenseTemplate | null>();
+
   licenseTemplates = linkedSignal(() => this.licenseTemplateData().data as LicenseTemplate[]);
-  openModal = signal(false);
-  selectedTemplate = signal<LicenseTemplate | null>(null);
 
-  modalEffect = effect(() => {
+  templateSelected = output<LicenseTemplate>();
+  newTemplate = output<void>();
 
-    if (this.openModal()) {
-      this.selectedTemplate.set(null);
-    }
-    
-    const licenseTemplate = this.selectedTemplate();
-    
-    if (licenseTemplate || this.openModal()) {
-      setTimeout(() => {
-        this.modalService.open('modal-license-template');
-      }, 500);
-    }
-  });
+  constructor() {
+    effect(() => {
+      const templateInformation = this.updatedTemplate();
+      if (templateInformation) {
+        this.updateTemplateList(templateInformation);
+      }
+    });
+  }
 
   selectTemplate(template: LicenseTemplate) {
-    this.selectedTemplate.set(template);
+    this.templateSelected.emit(template);
+  }
+
+  openModalNewLicenseTemplate() {
+    this.newTemplate.emit();
+  }
+
+  private updateTemplateList(templateInformation: LicenseTemplate) {
+    this.licenseTemplates.update((current) => {
+      const index = current.findIndex((term) => term.id === templateInformation.id);
+
+      if (index >= 0) {
+        current[index] = { ...current[index], ...templateInformation };
+        return [...current];
+      } else {
+        return [...current, templateInformation];
+      }
+    });
   }
 
   getActive(active: number) {
@@ -52,30 +62,5 @@ export class LicenseTemplateTableComponent {
     const sliceTerm = textTerm.slice(0, 100) + (textTerm.length > 100 ? '...' : '');
 
     return this.sanitizer.bypassSecurityTrustHtml(sliceTerm);
-  }
-
-  clearSelectedTemplate(templateInformation: LicenseTemplate) {
-  
-      if (templateInformation) {
-  
-        this.licenseTemplates.update((current) => {
-  
-          const index = current.findIndex((term) => term.id === templateInformation.id);
-  
-          if (index >= 0) {
-            current[index] = { ...current[index], ...templateInformation };
-            return [...current];
-          } else {
-            return [...current, templateInformation];
-          }
-        });
-      }
-      
-      this.selectedTemplate.set(null);
-    }
-
-
-  openModalNewLicenseTemplate(){
-    this.modalService.open('modal-license-template');
   }
 }
